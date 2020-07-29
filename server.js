6a8a216793b09fedd387f70a54f0541cee4363b4
Henrique Szabo <henrique.szabo@redfox.tech>
2020-07-29 19:28:00 -0300

    testing out

--- server.js
+++ server.js
@@ -7,4 +7,8 @@ app.get('/api/mensagem', (req, res) => {
   res.send({ express: 'Hello From Expressi' });
 });
 
+app.get('/', (req, res) => {
+    res.send({ express: 'Hello From Expresso' });
+  });
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
